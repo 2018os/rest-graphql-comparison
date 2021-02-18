c63772bed2ba3a0ce29f6052436e30a938762c1b
seungyeop Kim <seungyeop2002@gmail.com>
2021-02-18 19:58:15 +0900

    Init NestJS mono repo mode

--- apps/rest-api/src/main.ts
+++ apps/rest-api/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { RestApiModule } from './rest-api.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(RestApiModule);
   await app.listen(3000);
 }
 bootstrap();
--- apps/rest-api/src/rest-api.service.ts
+++ apps/rest-api/src/rest-api.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class RestApiService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello This is Rest API';
   }
 }
