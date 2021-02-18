fe37aec2cd8f84e1ff32a57b41eb2cdc0b6b7300
seungyeop Kim <seungyeop2002@gmail.com>
2021-02-19 01:44:46 +0900

    Remove graphql-api controllers & service

--- apps/graphql-api/src/graphql-api.module.ts
+++ apps/graphql-api/src/graphql-api.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
-import { GraphqlApiController } from './graphql-api.controller';
-import { GraphqlApiService } from './graphql-api.service';
 
 @Module({
   imports: [],
-  controllers: [GraphqlApiController],
-  providers: [GraphqlApiService],
 })
 export class GraphqlApiModule {}
--- apps/graphql-api/src/graphql-api.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class GraphqlApiService {
-  getHello(): string {
-    return 'Hello World!!!';
-  }
-}
