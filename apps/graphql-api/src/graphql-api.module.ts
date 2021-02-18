52866b627c3e2b728a47a239c7e0f068e5d44111
seungyeop Kim <seungyeop2002@gmail.com>
2021-02-19 01:47:04 +0900

    Init GraphQL Module & import AuthorsModule

--- apps/graphql-api/src/graphql-api.module.ts
+++ apps/graphql-api/src/graphql-api.module.ts
@@ -1,6 +1,16 @@
+import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
 
+import { AuthorsModule } from './authors/authors.module';
+
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'apps/graphql-api/src/schema.gql'),
+      sortSchema: true,
+    }),
+    AuthorsModule,
+  ],
 })
 export class GraphqlApiModule {}
