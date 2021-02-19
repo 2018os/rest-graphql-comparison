1edd9f37ae29226074ffcb24ce003fdeae987fd1
seungyeop Kim <seungyeop2002@gmail.com>
2021-02-19 19:04:09 +0900

    Apply on service test about remove 'async/await'

--- apps/graphql-api/src/authors/authors.service.spec.ts
+++ apps/graphql-api/src/authors/authors.service.spec.ts
@@ -18,18 +18,18 @@ describe('AuthorsService', () => {
   });
 
   describe('findAll', () => {
-    it('should return an array', async () => {
-      const result = await service.findAll();
+    it('should return an array', () => {
+      const result = service.findAll();
       expect(result).toBeInstanceOf(Array);
     });
   });
 
   describe('findOne', () => {
-    it('should return a author', async () => {
+    it('should return a author', () => {
       service.create({
         firstName: 'Test',
       });
-      const author = await service.findOneById(1);
+      const author = service.findOneById(1);
       expect(author).toBeDefined();
       expect(author.id).toEqual(1);
     });
@@ -44,18 +44,18 @@ describe('AuthorsService', () => {
   });
 
   describe('deleteOne', () => {
-    it('delete a author', async () => {
-      await service.create({
+    it('delete a author', () => {
+      service.create({
         firstName: 'Test',
       });
-      const beforeDelete = (await service.findAll()).length;
-      await service.delete(1);
-      const afterDelete = (await service.findAll()).length;
+      const beforeDelete = service.findAll().length;
+      service.delete(1);
+      const afterDelete = service.findAll().length;
       expect(afterDelete).toBeLessThan(beforeDelete);
     });
-    it('should throw NotFoundException Error', async () => {
+    it('should throw NotFoundException Error', () => {
       try {
-        await service.delete(999);
+        service.delete(999);
       } catch (e) {
         expect(e).toBeInstanceOf(NotFoundException);
         expect(e.message).toEqual('Not Found Author');
@@ -64,33 +64,33 @@ describe('AuthorsService', () => {
   });
 
   describe('create', () => {
-    it('should create a movie', async () => {
-      const beforeCreate = (await service.findAll()).length;
+    it('should create a movie', () => {
+      const beforeCreate = service.findAll().length;
       service.create({
         firstName: 'Test',
       });
-      const afterCreate = (await service.findAll()).length;
+      const afterCreate = service.findAll().length;
       expect(afterCreate).toBeGreaterThan(beforeCreate);
     });
   });
 
   describe('update', () => {
-    it('should update a author', async () => {
-      await service.create({
+    it('should update a author', () => {
+      service.create({
         firstName: 'Test',
       });
-      await service.update({
+      service.update({
         id: 1,
         firstName: 'Updated',
         lastName: 'Test',
       });
-      const author = await service.findOneById(1);
+      const author = service.findOneById(1);
       expect(author.firstName).toEqual('Updated');
       expect(author.lastName).toEqual('Test');
     });
-    it('should throw NotFoundException Error', async () => {
+    it('should throw NotFoundException Error', () => {
       try {
-        await service.update({
+        service.update({
           id: 999,
         });
       } catch (e) {
