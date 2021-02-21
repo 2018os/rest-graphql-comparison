f060126bf76a6671fe500ff255a0b373edaf0e43
seungyeop Kim <seungyeop2002@gmail.com>
2021-02-21 17:00:18 +0900

    Add e2e Test on graphql-api authors module

--- apps/graphql-api/test/app.e2e-spec.ts
+++ apps/graphql-api/test/app.e2e-spec.ts
@@ -1,10 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { GraphqlApiModule } from './../src/GraphqlApi.module';
+import { GraphqlApiModule } from '../src/graphql-api.module';
+import { Author } from '../src/authors/models/author.model';
 
 describe('GraphqlApiController (e2e)', () => {
   let app: INestApplication;
+  const GRAPHQL_ENDPOINT = '/graphql';
+
+  const send = (data?: Record<string, unknown>) =>
+    request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send(data);
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,10 +20,116 @@ describe('GraphqlApiController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterEach(() => {
+    app.close();
+  });
+
+  describe('Query about Author', () => {
+    describe('Query authors', () => {
+      it('should return an Array', (done) => {
+        send({
+          query: `
+            authors {
+              authors {
+                id
+              }
+            }
+          `,
+        })
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.data.authors).toBeInstanceOf(Array);
+          });
+        done();
+      });
+    });
+
+    describe('Query author', () => {
+      it('should return an Author', (done) => {
+        send({
+          query: `
+            author {
+              author(id: 1) {
+                id
+              }
+            }
+          `,
+        })
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.data.author).toBeInstanceOf(Author);
+          });
+        done();
+      });
+    });
+
+    describe('Mutation createAuthor', () => {
+      it('should return an Author id greater than 0', (done) => {
+        send({
+          mutation: `
+            createAuthor {
+              createAuthor(input: {}) {
+                id
+              }
+            }
+          `,
+        })
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.data.createAuthor.id).toBeGreaterThan(0);
+          });
+        done();
+      });
+
+      it('should return an Author about had default name', (done) => {
+        send({
+          mutation: `
+            createAuthor {
+              createAuthor(input: {}) {
+                firstName
+                lastName
+              }
+            }
+          `,
+        })
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.data.createAuthor.firstName).toBe('anonymous');
+            expect(res.body.data.createAuthor.lastName).toBe('author');
+          });
+        done();
+      });
+
+      it('should return an Author about had name', (done) => {
+        const firstName = 'Kim';
+        const lastName = '123';
+        send({
+          mutation: `
+            createAuthor {
+              createAuthor(input: {
+                firstName: "${firstName}",
+                lastName: "${lastName}"
+              })
+            }
+          `,
+        })
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.data.createAuthor.firstName).toBe(firstName);
+            expect(res.body.data.createAuthor.lastName).toBe(lastName);
+          });
+        done();
+      });
+
+      it.todo('failure create Author same name');
+    });
+
+    describe('Mutation updateAuthor', () => {
+      it.todo('Update author');
+    });
+
+    describe('Mutation deleteAuthor', () => {
+      it.todo('Delete author');
+    });
   });
 });
