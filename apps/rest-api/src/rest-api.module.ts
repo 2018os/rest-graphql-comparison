6bf622179608735f58ab3ed5bc506b67134858e3
seungyeop Kim <seungyeop2002@gmail.com>
2021-02-24 17:34:43 +0900

    Generate AuthorModule on rest-api app

--- apps/rest-api/src/rest-api.module.ts
+++ apps/rest-api/src/rest-api.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AuthorsModule } from './authors/authors.module';
 
-@Module({})
+@Module({
+  imports: [AuthorsModule],
+})
 export class RestApiModule {}
