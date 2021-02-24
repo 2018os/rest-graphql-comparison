90b253e16bf12f5f8cfa01186a8ead65f3957bb5
seungyeop Kim <seungyeop2002@gmail.com>
2021-02-24 17:34:03 +0900

    Remove default controller, service in rest-api app

--- apps/rest-api/src/rest-api.module.ts
+++ apps/rest-api/src/rest-api.module.ts
@@ -1,10 +1,4 @@
 import { Module } from '@nestjs/common';
-import { RestApiController } from './rest-api.controller';
-import { RestApiService } from './rest-api.service';
 
-@Module({
-  imports: [],
-  controllers: [RestApiController],
-  providers: [RestApiService],
-})
+@Module({})
 export class RestApiModule {}
